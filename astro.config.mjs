322da706fdd781c537044de9eba5291316f7fd02
Lautaro Rodriguez Brown <lautanba@gmail.com>
2024-12-24 23:04:08 -0300

    Instalación de dotenv

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import 'dotenv/config';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
